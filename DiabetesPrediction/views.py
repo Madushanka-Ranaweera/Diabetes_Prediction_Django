54ee9d893c3c18aa1faba697b6ae038e387fb310
Madushanka Ranaweera <67281217+Madushanka-Ranaweera@users.noreply.github.com>
2022-12-14 20:22:23 +0530

    Add files via upload

---
+++ DiabetesPrediction/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVC
+
+def home(request):
+    return render(request,'home.html')
+
+def predict(request):
+    return render(request,'predict.html')
+
+def result(request):
+    data = pd.read_excel(r'C:\Users\Madushanka Ranaweera\Desktop\Python\diabetes_DS.xlsx')
+
+    X = data.drop('Outcome',axis=1)
+    Y = data['Outcome']
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2, random_state=21)
+
+
+    SVM = SVC(kernel='rbf')
+    SVM.fit(X_train, Y_train)
+
+    val1 = float(request.GET['n1'])
+    val2 = float(request.GET['n2'])
+    val3 = float(request.GET['n3'])
+    val4 = float(request.GET['n4'])
+    val5 = float(request.GET['n5'])
+    val6 = float(request.GET['n6'])
+    val7 = float(request.GET['n7'])
+    val8 = float(request.GET['n8'])
+
+    pred = SVM.predict([[val1,val2,val3,val4,val5,val6,val7,val8]])
+
+    result1 = ""
+    if pred==[1]:
+        result1="Positive"
+    else:
+        result1="Negative"
+
+
+    return render(request,'predict.html', {"result2":result1})
